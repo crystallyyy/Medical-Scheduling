fd66e573eb5b6c4add451c90c552bc10fca40ea6
Erica Buck <ericambuck@icloud.com>
2023-05-31 18:43:32 -0500

    git test

--- final-capstone/capstone/vue/src/App.vue
+++ final-capstone/capstone/vue/src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;<!--| --> &nbsp;
+      <router-link v-bind:to="{ name: 'doctordash' }">Doctor Dashboard</router-link>&nbsp;<!--| --> &nbsp;
+      <router-link v-bind:to="{ name: 'patientdash' }">Patient Dashboard</router-link>&nbsp;<!--| --> &nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
