a795a05eacc84ca100f29545651e706108f928e5
Jonathan Jung <joonghan0201@gmail.com>
2023-06-01 16:25:27 -0400

    Reviews Draft

---
+++ final-capstone/capstone/vue/src/components/AverageSummary.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="well">
+    <span class="amount" v-on:click="updateFilter()">{{ averageRating }}</span>
+    Average Rating
+  </div>
+</template>
+
+<script>
+export default {
+  name: "average-summary",
+  methods: {
+    updateFilter() {
+      this.$store.commit("UPDATE_FILTER", 0);
+    }
+  },
+  computed: {
+    averageRating() {
+      const reviews = this.$store.getters.product.reviews;
+      let sum = reviews.reduce((currentSum, review) => {
+        return currentSum + review.rating;
+      }, 0);
+      if (sum === 0) {
+        return 0;
+      } else {
+        return (sum / reviews.length).toFixed(2);
+      }
+    }
+  }
+};
+</script>
---
+++ final-capstone/capstone/vue/src/views/AddReview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Add Review</h1>
+    <add-review />
+  </div>
+</template>
+
+<script>
+import AddReview from "../components/AddReview.vue";
+
+export default {
+  components: {
+    AddReview
+  }
+};
+</script>
+
+<style>
+</style>
