d03b6c0d87718ee2de93a3f96cdea5bea8d67f37
Nathanael Aninweze <nathanael.aninweze@outlook.com>
2023-06-04 16:35:53 -0400

    stashing changes

--- final-capstone/capstone/vue/src/components/CreateDoctor.vue
+++ final-capstone/capstone/vue/src/components/CreateDoctor.vue
@@ -1,76 +1,76 @@
 <template>
   <div>
     <form @submit.prevent="submitForm">
-        
       <div class="form-group">
         <label for="first_name">First Name:</label>
-        <input type="text" class="form-control" v-model="firstName" required>
+        <input type="text" class="form-control" v-model="firstName" required />
       </div>
-      
+
       <div class="form-group">
         <label for="last_name">Last Name:</label>
-        <input type="text" class="form-control" v-model="lastName" required>
+        <input type="text" class="form-control" v-model="lastName" required />
       </div>
-      
+
       <div class="form-group">
         <label for="time_slot_default">Time Slot Default:</label>
-        <input type="text" class="form-control" v-model="timeSlotDefault" required>
+        <input
+          type="text"
+          class="form-control"
+          v-model="timeSlotDefault"
+          required
+        />
       </div>
-      
+
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" class="form-control" v-model="email" required>
+        <input type="email" class="form-control" v-model="email" required />
       </div>
-      
+
       <button type="submit" class="btn btn-primary">Submit</button>
-      
     </form>
-    
   </div>
 </template>
 
 <script>
 import providerService from "../services/providerService.js";
 export default {
-    props :{
-        userName: String
-    },
+  props: {
+    userName: String,
+  },
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      timeSlotDefault: '',
-      email: '',
+      firstName: "",
+      lastName: "",
+      timeSlotDefault: "",
+      email: "",
       userId: 0,
       doctor: {},
     };
   },
   methods: {
-    async submitForm() { 
-          try {
-    const res1 = await providerService.getDoctorUserIdByUsername(this.userName);
-    this.userId = res1.data;
-    const res2 = await providerService.getDoctorByUserId(this.userId);
-    this.doctor = res2.data;
-    this.doctor.firstName = this.firstName;
-    this.doctor.lastName = this.lastName;
-    this.doctor.email = this.email;
-    this.doctor.timeSlotDefault = this.timeSlotDefault;
-    providerService.updateDoctorInformation(this.doctor).then(res=>{
-      if(res.status==200){
-        this.$router.push("/doctorDash")
+    async submitForm() {
+      try {
+        const res1 = await providerService.getDoctorUserIdByUsername(
+          this.userName
+        );
+        this.userId = res1.data;
+        const res2 = await providerService.getDoctorByUserId(this.userId);
+        this.doctor = res2.data;
+        this.doctor.firstName = this.firstName;
+        this.doctor.lastName = this.lastName;
+        this.doctor.email = this.email;
+        this.doctor.timeSlotDefault = this.timeSlotDefault;
+        providerService.updateDoctorInformation(this.doctor).then((res) => {
+          if (res.status == 200) {
+            this.$router.push("/doctorDash");
+          }
+        });
+      } catch (error) {
+        // Handle any errors that occur during the async calls
+        console.error(error);
       }
-    })
-       } catch (error) {
-    // Handle any errors that occur during the async calls
-    console.error(error);
-  }
-   
     },
-    created(){
-      
-   
-    }
-  }
+    created() {},
+  },
 };
 </script>
