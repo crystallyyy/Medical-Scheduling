e70506b457755b9e54c8edda7d9854789d888a52
Jonathan Jung <joonghan0201@gmail.com>
2023-06-05 22:34:46 -0400

    availability, dashboard, etc

--- final-capstone/capstone/vue/src/views/DoctorDashboard.vue
+++ final-capstone/capstone/vue/src/views/DoctorDashboard.vue
@@ -1,50 +1,47 @@
 <template>
-  <div id="main">
+<div id='main'>
     <div id="nav">
       <div class="imgdiv">
-        <img id="img" src="../../img/logo2.jpeg" alt="company logo" />
+          <img id="img" src="../../img/logo2.jpeg" alt="company logo">
       </div>
       <ul class="nav flex-column">
+
+          
         <li class="nav-item">
-          <button type="button" class="btn btn-info" @click="activeCard = 1">
-            Dashboard
-          </button>
+          <button type="button" class="btn btn-info" @click="activeCard = 1">Dashboard</button>
+
         </li>
         <li class="nav-item">
-          <button type="button" class="btn btn-info" @click="activeCard = 2">
-            My Profile
-          </button>
+          <button type="button" class="btn btn-info " @click="activeCard = 2">My Profile</button>
         </li>
         <li class="nav-item">
-          <button type="button" class="btn btn-info" @click="activeCard = 3">
-            Manage Locations
-          </button>
+          <button type="button" class="btn btn-info" @click="activeCard = 3">Manage Locations</button>
         </li>
         <li class="nav-item">
-          <button type="button" class="btn btn-info" @click="activeCard = 4">
-            Manage Availability
-          </button>
+          <button type="button" class="btn btn-info" @click="activeCard = 4">Manage Availability</button>
         </li>
+
       </ul>
     </div>
 
-    <div id="content">
-      <div class="card card-body" v-show="activeCard === 1">
-        Content for show dashboard
-      </div>
-      <div class="card card-body" v-show="activeCard === 2">
-        Content for my profile
-      </div>
-      <div class="card card-body" v-show="activeCard === 3">
-        Content for manage location
-      </div>
-      <div class="card card-body" v-show="activeCard === 4">
-        Content for change availability
-      </div>
+<div id="content"> 
+    <div class="card card-body" v-show="activeCard === 1">
+          Content for show dashboard
     </div>
-  </div>
-
+    <div class="card card-body" v-show="activeCard === 2">
+          Content for my profile
+    </div>  
+    <div class="card card-body" v-show="activeCard === 3">
+          Content for manage location
+    </div>
+    <div class="card card-body" v-show="activeCard === 4">
+          Content for change availability
+    </div> 
+</div>
+</div>
+    
   <!-- include contact support -->
+  
 </template>
 
 <script>
@@ -52,14 +49,18 @@ export default {
   name: "doctor-dash",
   data() {
     return {
-      activeCard: null,
-      methods: {},
-    };
-  },
-};
+
+    activeCard: null,
+    methods:{
+
+     } 
+    }
+  }
+}
 </script>
 
 <style scoped>
+    
 #main {
   display: flex;
 }
@@ -112,4 +113,81 @@ export default {
   display: block;
 }
 
+
+     /* #main{
+        
+        display: grid;
+        height: 90vh;
+        grid-template-columns: 25% 75%;
+        grid-template-areas: "nav content";
+        margin: 20px;
+        border: 4px solid black;
+    }
+
+    #nav{
+        grid-area: nav;
+        display:flex;
+        flex-direction: column;
+        height: 100%;
+        background-color: #243688;
+      
+    }
+
+    #content{
+        grid-area: content;
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        height: 100%;
+        background-color: whitesmoke;
+    }
+      
+    #img {
+      max-width: 100px;
+      margin-bottom: 20px;
+      display: flex;
+      margin: auto;
+    }
+
+ */
+
+
+
+
+    /* #img {
+      max-width: 30%;
+      margin-bottom: 20px;
+    }
+    #main{
+        display: grid;
+        grid-template-columns: 25% 75%;
+        grid-template-areas: "nav content";
+    }
+    #nav{
+    grid-area: nav;
+    background-color: grey;
+    display:flex;
+    justify-content: center;
+    flex-direction: column;
+    align-content: center;
+    }
+    #content{
+      grid-area: content;
+        display: flex;
+    }
+    .imgdiv {
+    flex-basis: 25%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    }
+
+    #nav ul {
+  flex-grow: 1;
+   list-style-type: none;
+   margin: 0;
+   padding:0;
+   justify-content: center;
+
+    } */
 </style>
