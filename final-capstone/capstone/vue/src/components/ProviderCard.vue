1370053bc86b484a9ddf2ad39fb86c066aad626c
Nathanael Aninweze <nathanael.aninweze@outlook.com>
2023-06-06 22:30:33 -0400

    commiting

---
+++ final-capstone/capstone/vue/src/components/ProviderCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+     <h2>Dr. {{ doctor.firstName }} {{ doctor.lastName }}</h2>
+      <h3>{{ doctor.email }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+props:{
+    doctor: Object,
+}
+}
+</script>
+
+<style>
+
+</style>
