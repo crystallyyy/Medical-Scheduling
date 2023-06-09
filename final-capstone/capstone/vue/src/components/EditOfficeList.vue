a6eb3c7edff2f5dacf939b11149efbed578bf3e6
Jonathan Jung <joonghan0201@gmail.com>
2023-06-08 22:57:03 -0400

    EditOfficeList functions

--- final-capstone/capstone/vue/src/components/EditOfficeList.vue
+++ final-capstone/capstone/vue/src/components/EditOfficeList.vue
@@ -15,11 +15,16 @@
         <tbody>
           <tr v-for="office in offices" v-bind:key="office.officeId">
             <td>{{ office.officeName }}</td>
-            <td><a>{{ office.address }}</a></td>
+            <td>
+              <a>{{ office.address }}</a>
+            </td>
             <td>{{ office.phoneNumber }}</td>
 
             <td>
-              <button @click="showFormWithData(office)">
+              <button
+                id="editOfficeinfoButton"
+                @click="showFormWithData(office)"
+              >
                 <strong>Edit Office Information</strong>
               </button>
             </td>
@@ -27,29 +32,31 @@
         </tbody>
       </table>
     </div>
-   
 
     <div class="updateform">
       <form @submit.prevent="submitForm" v-show="showForm">
         <div id="officeinfoFrame" class="officeinfo">
-          <h2>Office Info</h2>
+          <h2>
+            <strong><u>Office Info</u></strong>
+          </h2>
           <div id="officeNameFrame">
             <label for="officeName">Office Name: &nbsp;&nbsp;</label>
             <input type="text" id="officeName" v-model="officeName" required />
           </div>
-          <div id="addressFrame" >
+          <div id="addressFrame">
             <label for="address">Address: &nbsp;&nbsp;</label>
             <input type="text" id="address" v-model="address" required />
           </div>
-          <div id="phoneNumberFrame" >
+          <div id="phoneNumberFrame">
             <label for="phoneNumber">Phone Number: &nbsp;&nbsp;</label>
             <input type="text" id="phoneNumber" v-model="phoneNumber" />
           </div>
         </div>
-      
 
-        <div class="office hours">
-          <h2>Office Hours</h2>
+        <div id="officehourFrame" class="office hours">
+          <h2>
+            <strong><u>Office Hours</u></strong>
+          </h2>
           <div id="mondayFrame">
             <label for="Mondaystart">Monday: &nbsp;&nbsp;</label>
             <input
@@ -57,7 +64,8 @@
               id="Mondaystart"
               v-model="officeHours[0].startTime"
               required
-            />  &nbsp;&nbsp;
+            />
+            &nbsp;&nbsp;
             <label for="Mondayend"></label>
             <input
               type="time"
@@ -74,7 +82,8 @@
               id="Tuesdaystart"
               v-model="officeHours[1].startTime"
               required
-            /> &nbsp;&nbsp;
+            />
+            &nbsp;&nbsp;
             <label for="Tuesdayend"></label>
             <input
               type="time"
@@ -90,7 +99,8 @@
               id="Wednesdaystart"
               v-model="officeHours[2].startTime"
               required
-            /> &nbsp;&nbsp;
+            />
+            &nbsp;&nbsp;
             <label for="Wednesdayend"></label>
             <input
               type="time"
@@ -106,7 +116,8 @@
               id="Thursdaystart"
               v-model="officeHours[3].startTime"
               required
-            /> &nbsp;&nbsp;
+            />
+            &nbsp;&nbsp;
             <label for="Thursdayend"></label>
             <input
               type="time"
@@ -122,7 +133,8 @@
               id="Fridaystart"
               v-model="officeHours[4].startTime"
               required
-            /> &nbsp;&nbsp;
+            />
+            &nbsp;&nbsp;
             <label for="Fridayend"></label>
             <input
               type="time"
@@ -133,46 +145,52 @@
           </div>
         </div>
         <div id="submitUpdateButtonFrame">
-          <button id="submitUpdateButton" type="submit" >Update</button>
+          <button id="submitUpdateButton" type="submit">Update</button>
+        </div>
+
+        <div class="accordion" role="tablist">
+          <b-card no-body class="mb-1">
+            <div>
+              <b-button
+                id="updateInformationButton"
+                block
+                v-b-toggle.accordion-1
+                variant="info"
+                >Add Doctor (Click to Open)</b-button
+              >
+            </div>
+            <b-collapse
+              id="accordion-1"
+              accordion="my-accordion"
+              role="tabpanel"
+            >
+              <div id="paragraph">
+                <p>Please select from the list</p>
+              </div>
+              <div id="selectFrame">
+                <select v-model="selectedDoctorId">
+                  <option
+                    v-for="option in options"
+                    :key="option.doctorId"
+                    :value="option.doctorId"
+                  >
+                    {{ option.firstName }} {{ option.lastName }}
+                  </option>
+                </select>
+              </div>
+              <div id="submitButtonFrame">
+                <button id="submitButton" @click="addProvider()">SUBMIT</button>
+              </div>
+              <b-card-body> </b-card-body>
+            </b-collapse>
+          </b-card>
         </div>
       </form>
-
-      <!-- <div id="providersAvailableFrame" v-show="showForm">
-        <div class="providersAvailable">
-          <h2>Providers Available</h2>
-          <div class="doctors"
-              v-for="doctor in doctorsInOffice"
-              v-bind:key="doctor.firstName" >
-            <p> {{ doctor.firstName }} {{ doctor.lastName }} </p>
-            <button @click="removeProvider(doctor)"> REMOVE </button>
-          </div>
-        </div>
-      </div> -->
-
-        <div id="addDoctorButtonFrame" >
-          <b-button id="addDoctorButton" size="sm" v-b-toggle="'collapse-2'" class="m-1" >Add</b-button>
-          <div>
-    <!-- Element to collapse -->
-          <b-collapse id="collapse-2">
-            <b-card>
-              <p>Please select from the list</p>
-              <select  v-model="selectedDoctorId">
-                
-                <option v-for="option in options" :key="option.doctorId" :value="option.doctorId">
-                  {{option.firstName}} {{option.lastName}}
-                </option>
-              </select>
-              <button @click="addProvider()"> SUBMIT </button>
-            </b-card>
-          </b-collapse> 
-        </div> 
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 import officeService from "../services/officeService.js";
 import providerService from "../services/providerService.js";
 
@@ -183,7 +201,7 @@ export default {
       offices: [],
       officeHours: [],
       doctorsInOffice: [],
-      options:[],
+      options: [],
       removedDocId: null,
       selectedDoctorId: null,
       showForm: false,
@@ -208,9 +226,9 @@ export default {
           this.doctorsInOffice = response.data;
         });
 
-        providerService.getAllProviders().then((response)=> {
+        providerService.getAllProviders().then((response) => {
           this.options = response.data;
-        })
+        });
       });
     });
   },
@@ -225,6 +243,11 @@ export default {
       this.showForm = false;
     },
     showFormWithData(office) {
+      
+      // trying to re-collapse "Edit Office Inofrmation" from MANAGE LOCATIONS 
+      if (this.activeOffice.officeId === office.officeId) {
+    this.showForm = false;
+  }
       this.showForm = true;
       this.activeOffice = office;
       this.officeId = office.officeId;
@@ -241,33 +264,30 @@ export default {
       });
     },
 
-    updateHours(){
-      this.officeHours.forEach((officehour)=> {
+    updateHours() {
+      this.officeHours.forEach((officehour) => {
         officeService.updateOfficeHours(officehour);
       });
     },
 
-    addProvider(){
-      
-      officeService.addDoctorToOffice(this.selectedDoctorId,this.activeOffice.officeId);
+    addProvider() {
+      officeService.addDoctorToOffice(
+        this.selectedDoctorId,
+        this.activeOffice.officeId
+      );
     },
-    removeProvider(provider){
-
-      officeService.removeDoctorFromOffice(provider.doctorId,this.activeOffice.officeId);
-    }
+    removeProvider(provider) {
+      officeService.removeDoctorFromOffice(
+        provider.doctorId,
+        this.activeOffice.officeId
+      );
+    },
   },
 };
 </script>
 
+
 <style scoped>
-
-/* #officeinfoFrame {
-  display: flex;
-  justify-content: center;
-
-} */
-
-
 .officeTable {
   margin-left: auto;
   margin-right: auto;
@@ -292,15 +312,15 @@ export default {
   background-color: #e6e6e6;
 }
 
-
 h2 {
   text-align: center;
+  margin-bottom: 20px;
 }
 
-.mainoffice{
+.mainoffice {
   display: flex;
   flex-direction: column;
-  background-color: blueviolet;
+  /* background-color: blueviolet; */
   justify-content: center;
 }
 .actions {
@@ -338,7 +358,7 @@ h3 {
 }
 
 .officeTable tbody tr:nth-child(even) {
-  background-color: #f9f9f9;
+  /* background-color: #f9f9f9; */
 }
 
 .officeTable tbody tr:hover {
@@ -372,20 +392,19 @@ button:hover {
   padding: 20px;
 }
 
-.updateform{
+/* .updateform{
   display: flex;
-  /* justify-content: center; */
   flex-direction: column;
   width: 100%;
 
   max-height: 50%;
   background-color: whitesmoke;
   margin-top: 30px;
-}
+} */
 
 .updateform h2 {
-  background-color: darkolivegreen;
-  font-size: 1em;
+  /* background-color: darkolivegreen; */
+  font-size: 1.35em;
 }
 
 .officehours {
@@ -393,15 +412,10 @@ button:hover {
   display: flex;
 }
 
-/* #providersAvailable{
+#providersAvailableFrame {
   display: flex;
   justify-content: center;
-} */
-
-#providersAvailableFrame{
-  display: flex;
-  justify-content: center;
-  background-color: seagreen;
+  background-color: rgb(46, 83, 139);
 }
 
 .providersAvailable button {
@@ -413,7 +427,6 @@ button:hover {
   flex-basis: 50%;
   align-content: center;
   background-color: burlywood;
-  
 }
 .doctors p {
   background-color: cadetblue;
@@ -421,43 +434,43 @@ button:hover {
 }
 .doctors button {
   margin-left: 10px;
-  padding:5px;
+  padding: 5px;
 }
 
 #officeNameFrame {
-  margin-left: 240px;
+  margin-left: 610px;
 }
 
 #addressFrame {
-  margin-left: 272px;
+  margin-left: 642px;
 }
 
 #phoneNumberFrame {
-  margin-left: 221px;
+  margin-left: 591px;
 }
 
 #mondayFrame {
-  margin-left: 288px;
+  margin-left: 637px;
   margin-bottom: 8px;
 }
 
 #tuesdayFrame {
-  margin-left: 288px;
+  margin-left: 637px;
   margin-bottom: 8px;
 }
 
 #wednesdayFrame {
-  margin-left: 265px;
+  margin-left: 614px;
   margin-bottom: 8px;
 }
 
 #thursdayFrame {
-  margin-left: 282px;
+  margin-left: 631px;
   margin-bottom: 8px;
 }
 
 #fridayFrame {
-  margin-left: 304px;
+  margin-left: 653px;
   margin-bottom: 8px;
 }
 
@@ -473,8 +486,7 @@ button:hover {
 
 #addDoctorButtonFrame {
   display: flex;
-  justify-content: center;
-  
+  margin-left: 441px;
 }
 
 p {
@@ -493,6 +505,70 @@ p {
 
 #submitUpdateButtonFrame {
   display: flex;
-  margin-left: 445px;
+  /* margin-left: 445px; */
+  justify-content: center;
+}
+
+#selectFrame {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+#submitButtonFrame {
+  display: flex;
+  justify-content: center;
+}
+
+#submitButton {
+  background-color: #04abcc;
+  margin-bottom: 30px;
+}
+
+#submitButton:hover {
+  background-color: #048eaa;
+}
+
+#buttonHeader {
+  display: flex;
+  width: 50%;
+  /* margin-bottom: 20px */
+}
+
+#updateInformationButton {
+  background-color: #04abcc;
+  /* margin-bottom: 20px; */
+}
+
+#updateInformationButton.hover {
+  background-color: #048eaa;
+}
+
+.card.mb-1 {
+  display: flex;
+  justify-content: center;
+  /* width: 22%; */
+  /* flex-direction: row */
+}
+
+.accordion {
+  display: flex;
+  justify-content: center;
+}
+
+#paragraph {
+  margin-top: 15px;
+}
+
+#officeinfoFrame {
+  margin-top: 40px;
+}
+
+#officehourFrame {
+  margin-top: 40px;
+}
+
+#editOfficeinfoButton {
+  color: #04abcc;
 }
 </style>
--- final-capstone/capstone/vue/src/components/UpdateInfoPD.vue
+++ final-capstone/capstone/vue/src/components/UpdateInfoPD.vue
@@ -14,97 +14,111 @@
 
     <div class="accordion" role="tablist">
       <b-card no-body class="mb-1">
-        <b-card-header id="buttonHeader" header-tag="header" class="p-1" role="tab">
-        <b-button id="updateInformationButtonFrame" block v-b-toggle.accordion-1 variant="info">Update Information (Click to Open)</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <div id="patient-info-update-form">
-          <div id="firstNameFrame">
-            <label for="firstName">First Name: &nbsp;&nbsp;</label>
-            <input
-              type="text"
-              name="firstName"
-              id="firstName"
-              v-model="updatedPatient.firstName"
-              required
-            />
-          </div>
-          <div id="lastNameFrame">
-            <label for="lastName">Last Name: &nbsp;&nbsp;</label>
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              v-model="updatedPatient.lastName"
-              required
-            />
-          </div>
-          <div id="dateOfBirthFrame">
-            <label for="dateOfBirth">Date of Birth: &nbsp;&nbsp;</label>
-            <input
-              type="date"
-              name="dateOfBirth"
-              id="dateOfBirth"
-              v-model="updatedPatient.dateOfBirth"
-              required
-            />
-          </div>
-          <div id="address1Frame">
-            <label for="address_line1">Address Line 1: &nbsp;&nbsp;</label>
-            <input
-              type="text"
-              id="address_line1"
-              v-model="address.address_line1"
-              required
-            />
-          </div>
-          <div id="address2Frame">
-            <label for="address_line2">Address Line 2: &nbsp;&nbsp;</label>
-            <input
-              type="text"
-              id="address_line2"
-              v-model="address.address_line2"
-            />
-          </div>
-          <div id="cityFrame">
-            <label for="city">City: &nbsp;&nbsp;</label>
-            <input type="text" id="city" v-model="address.city" required />
-          </div>
-          <div id="stateFrame">
-            <label for="state">State: &nbsp;&nbsp;</label>
-            <input type="text" id="state" v-model="address.state" required />
-          </div>
-          <div id="zipCodeFrame">
-            <label for="zip_code">Zip Code: &nbsp;&nbsp;</label>
-            <input
-              type="text"
-              id="zip_code"
-              v-model="address.zip_code"
-              required
-            />
-          </div>
-          <div id="phoneNumberFrame">
-            <label for="phoneNumber">Phone Number: &nbsp;&nbsp;</label>
-            <input
-              type="text"
-              name="phoneNumber"
-              id="phoneNumber"
-              v-model="updatedPatient.phoneNumber"
-            />
-          </div>
-          <div id="submitButtonFrame">
-            <button id="submitButton" type="submit" v-on:click="submitForm">
-              Submit
-            </button>
-          </div>
-        </div>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-
+        <b-card-header
+          id="buttonHeader"
+          header-tag="header"
+          class="p-1"
+          role="tab"
+        >
+          <b-button
+            id="updateInformationButtonFrame"
+            block
+            v-b-toggle.accordion-1
+            variant="info"
+            >Update Information (Click to Open)</b-button
+          >
+        </b-card-header>
+        <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
+          <b-card-body>
+            <div id="patient-info-update-form">
+              <div id="firstNameFrame">
+                <label for="firstName">First Name: &nbsp;&nbsp;</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  v-model="updatedPatient.firstName"
+                  required
+                />
+              </div>
+              <div id="lastNameFrame">
+                <label for="lastName">Last Name: &nbsp;&nbsp;</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  v-model="updatedPatient.lastName"
+                  required
+                />
+              </div>
+              <div id="dateOfBirthFrame">
+                <label for="dateOfBirth">Date of Birth: &nbsp;&nbsp;</label>
+                <input
+                  type="date"
+                  name="dateOfBirth"
+                  id="dateOfBirth"
+                  v-model="updatedPatient.dateOfBirth"
+                  required
+                />
+              </div>
+              <div id="address1Frame">
+                <label for="address_line1">Address Line 1: &nbsp;&nbsp;</label>
+                <input
+                  type="text"
+                  id="address_line1"
+                  v-model="address.address_line1"
+                  required
+                />
+              </div>
+              <div id="address2Frame">
+                <label for="address_line2">Address Line 2: &nbsp;&nbsp;</label>
+                <input
+                  type="text"
+                  id="address_line2"
+                  v-model="address.address_line2"
+                />
+              </div>
+              <div id="cityFrame">
+                <label for="city">City: &nbsp;&nbsp;</label>
+                <input type="text" id="city" v-model="address.city" required />
+              </div>
+              <div id="stateFrame">
+                <label for="state">State: &nbsp;&nbsp;</label>
+                <input
+                  type="text"
+                  id="state"
+                  v-model="address.state"
+                  required
+                />
+              </div>
+              <div id="zipCodeFrame">
+                <label for="zip_code">Zip Code: &nbsp;&nbsp;</label>
+                <input
+                  type="text"
+                  id="zip_code"
+                  v-model="address.zip_code"
+                  required
+                />
+              </div>
+              <div id="phoneNumberFrame">
+                <label for="phoneNumber">Phone Number: &nbsp;&nbsp;</label>
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  id="phoneNumber"
+                  v-model="updatedPatient.phoneNumber"
+                />
+              </div>
+              <div id="submitButtonFrame">
+                <button id="submitButton" type="submit" v-on:click="submitForm">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
     </div>
-
   </div>
 </template>
 
@@ -268,10 +282,10 @@ export default {
   background-color: #048eaa;
 }
 
-.mb-1{
+.mb-1 {
   display: flex;
   justify-content: center;
-  width: 50%
+  width: 50%;
 }
 
 .accordion {
