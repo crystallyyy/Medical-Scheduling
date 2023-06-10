dd2238c8bfd7008ead61a4301255984e743ba2b1
Crystal Almaraz <133157601+crystallyyy@users.noreply.github.com>
2023-06-10 13:18:20 -0700

    Update create.sh

--- final-capstone/capstone/java/database/create.sh
+++ final-capstone/capstone/java/database/create.sh
@@ -2,8 +2,18 @@
 export PGPASSWORD='postgres1'
 BASEDIR=$(dirname $0)
 DATABASE=medical-db
-psql -U postgres -f "$BASEDIR/dropdb.sql" &&
-createdb -U postgres $DATABASE &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
+for i in 18 17 16 15 14 13 12 11 10 9
+do
+    file="c:/program files/postgresql/$i/"
+    FILEPATH="c:/program files/postgresql/$i/bin"
+    if [ -d "$file" ]  
+    then 
+        echo "Postgres version $i found."
+        "$FILEPATH/psql.exe" -U postgres -f "$BASEDIR/dropdb.sql" &&
+        "$FILEPATH/createdb.exe" -U postgres $DATABASE &&
+        "$FILEPATH/psql.exe" -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
+        "$FILEPATH/psql.exe" -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
+        "$FILEPATH/psql.exe" -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
+        break
+    fi 
+done
